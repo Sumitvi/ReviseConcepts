408f8de7eb5583e4d23aac7da433f61bacf4b852
Sumit Vishwakarma <sv0042595@gmail.com>
2025-12-12 13:14:13 +0530

    REVISEING CONCEPTS

---
+++ src/main/java/org/example/corejava/Basic.java
@@ -0,0 +1,13 @@
+package org.example.corejava;
+
+public class Basic {
+
+    public static void main(String args[]){
+
+        System.out.println("Hello Sumit");
+
+
+//        Operators
+        
+    }
+}
