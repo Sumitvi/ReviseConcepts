1f484349b19a04364a243b5a2df30935f00da7f2
Sumit Vishwakarma <sv0042595@gmail.com>
2025-12-12 14:59:25 +0530

    From Basics

--- src/main/java/org/example/corejava/LoopsDemo.java
+++ src/main/java/org/example/corejava/LoopsDemo.java
@@ -4,6 +4,10 @@ public class LoopsDemo {
 
     public static void main(String args[]){
 
-        
+//        For Loop
+
+        for(int i=0; i<10; i++){
+            System.out.println(i);
+        }
     }
 }
