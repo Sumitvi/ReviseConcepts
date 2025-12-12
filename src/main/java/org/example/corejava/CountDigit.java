0d7c0732dab5e176ab06220c6559ab6292ff6412
Sumit Vishwakarma <sv0042595@gmail.com>
2025-12-12 21:58:42 +0530

    Basic Revision

--- src/main/java/org/example/corejava/CountDigit.java
+++ src/main/java/org/example/corejava/CountDigit.java
@@ -8,8 +8,15 @@ public class CountDigit {
         System.out.println("Enter a Number");
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
+        int count = 0;
 
-        
+        while(num > 0){
+            num = num / 10;
+            count++;
+        }
+
+        System.out.println(count);
+
 
     }
 }
