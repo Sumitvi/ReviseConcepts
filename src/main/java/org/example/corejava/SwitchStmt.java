097148b308e43cd34a482312b4959c8519d03a00
Sumit Vishwakarma <sv0042595@gmail.com>
2025-12-12 22:05:55 +0530

    Basic Revision

---
+++ src/main/java/org/example/corejava/SwitchStmt.java
@@ -0,0 +1,25 @@
+package org.example.corejava;
+
+public class SwitchStmt {
+
+    public static void main(String args[]){
+
+        int age = 30;
+
+        switch (age){
+            case 10:
+                System.out.println("Age is 10 ");
+                break;
+
+            case 20:
+                System.out.println("Age is 20 ");
+                break;
+
+            case 30:
+                System.out.println("Age is 30 ");
+                break;
+
+        }
+
+    }
+}
