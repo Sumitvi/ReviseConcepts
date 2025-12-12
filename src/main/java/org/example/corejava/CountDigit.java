7788986b83b0853a9b415da1da4e6a64a3aff93d
Sumit Vishwakarma <sv0042595@gmail.com>
2025-12-12 21:57:33 +0530

    Basic Revision

---
+++ src/main/java/org/example/corejava/CountDigit.java
@@ -0,0 +1,15 @@
+package org.example.corejava;
+import java.util.*;
+
+public class CountDigit {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter a Number");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        
+
+    }
+}
