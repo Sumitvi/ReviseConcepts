902b40772ea0b146c2710f13f916501337fd5cd1
Sumit Vishwakarma <sv0042595@gmail.com>
2025-12-12 14:53:35 +0530

    Revise

--- src/main/java/org/example/corejava/Basic.java
+++ src/main/java/org/example/corejava/Basic.java
@@ -8,11 +8,28 @@ public class Basic {
 
 
 //        Operators
-        int a = 20;
-        int b = 10;
+//        int a = 20;
+//        int b = 10;
+//
+//        System.out.println("Addition");
+//        System.out.println(a + b);
+//        System.out.println(a * b);
+//        System.out.println(a / b);
+//        System.out.println(a % b);
+//        System.out.println(a++);
+//        System.out.println(a);
+//        System.out.println(a--);
+//        System.out.println(a);
 
-        System.out.println("Addition");
-        System.out.println(a + b);
+
+//        Ternary Operator
+
+        int one = 1;
+        int two = 2;
+
+
+        String max = (one < two) ? "Yes": "No";
+        System.out.println(max);
 
     }
 }
---
+++ src/main/java/org/example/corejava/ControlStmt.java
@@ -0,0 +1,18 @@
+package org.example.corejava;
+import java.util.*;
+
+public class ControlStmt {
+
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+
+        if(age > 18){
+            System.out.println("You Can Vote");
+        }else{
+            System.out.println("You Can'nt Vote");
+        }
+
+    }
+}
