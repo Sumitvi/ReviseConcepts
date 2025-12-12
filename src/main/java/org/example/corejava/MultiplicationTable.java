82d0d01807f3caf07c8a2084613680659e95e1be
Sumit Vishwakarma <sv0042595@gmail.com>
2025-12-12 21:51:21 +0530

    Basic Revision

--- src/main/java/org/example/corejava/MultiplicationTable.java
+++ src/main/java/org/example/corejava/MultiplicationTable.java
@@ -6,10 +6,11 @@ public class MultiplicationTable {
     public static void main(String args[]){
 
 //      Print multiplication table of a given number
+        System.out.println("Enter value of n ");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        for(int i=1; i<=n; i++){
+        for(int i=1; i<=10; i++){
             System.out.println(n + "*" + i + "=" + n * i);
         }
 
