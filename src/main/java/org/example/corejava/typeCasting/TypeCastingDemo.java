4a3ad2390b2b7db338a3b6494eb54f809585e9ce
Sumit Vishwakarma <sv0042595@gmail.com>
2025-12-13 14:22:05 +0530

    Type Casting

--- src/main/java/org/example/corejava/typeCasting/TypeCastingDemo.java
+++ src/main/java/org/example/corejava/typeCasting/TypeCastingDemo.java
@@ -6,5 +6,13 @@ public class TypeCastingDemo {
 //        Process of converting one type to another is called type casting
 //        Widening - from lower data type to higher
 //        Narrowing - from higher to lower data type
+
+//        Widening Example
+        int a = 10;
+//        System.out.println(a);
+        double data = a;
+        System.out.println(a);
+
+
     }
 }
