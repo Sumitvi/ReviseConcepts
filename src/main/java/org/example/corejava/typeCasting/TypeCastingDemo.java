f1c8e9d6776f37537988413b8eda426e1c270fca
Sumit Vishwakarma <sv0042595@gmail.com>
2025-12-13 14:20:10 +0530

    OOPS Concepts

---
+++ src/main/java/org/example/corejava/typeCasting/TypeCastingDemo.java
@@ -0,0 +1,10 @@
+package org.example.corejava.typeCasting;
+
+public class TypeCastingDemo {
+    public static void main(String args[]){
+
+//        Process of converting one type to another is called type casting
+//        Widening - from lower data type to higher
+//        Narrowing - from higher to lower data type
+    }
+}
