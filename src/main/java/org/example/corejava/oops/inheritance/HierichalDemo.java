0b144f16d73b904757884f56e1f8a0414c8e9461
Sumit Vishwakarma <sv0042595@gmail.com>
2025-12-13 13:27:50 +0530

    OOPS Concepts

---
+++ src/main/java/org/example/corejava/oops/inheritance/HierichalDemo.java
@@ -0,0 +1,27 @@
+package org.example.corejava.oops.inheritance;
+
+class VehicleDemo{
+
+    public void run(){
+        System.out.println("Vehicle is Running");
+    }
+
+}
+
+
+class Car extends VehicleDemo{
+    public static void type(){
+        System.out.println("Car");
+    }
+}
+
+class 
+
+public class HierichalDemo {
+
+    public static void main(String[] args) {
+
+//        Hierarchical Inheritance
+
+    }
+}
