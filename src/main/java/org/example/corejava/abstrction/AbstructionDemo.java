8bf90f8751b78e4a6f2499dca174d1ec58dd9e80
Sumit Vishwakarma <sv0042595@gmail.com>
2025-12-13 13:46:50 +0530

    OOPS Concepts

---
+++ src/main/java/org/example/corejava/abstrction/AbstructionDemo.java
@@ -0,0 +1,40 @@
+package org.example.corejava.abstrction;
+
+
+abstract class Vehicle{
+    abstract void start();
+
+    public void stop(){
+        System.out.println("Vehicle Stop");
+    }
+}
+
+class Car extends Vehicle{
+    @Override
+    void start(){
+        System.out.println("Car Start by Key");
+    }
+}
+
+class Bike extends Vehicle{
+    @Override
+    void start(){
+        System.out.println("Bike Start by Kick");
+    }
+}
+
+public class AbstructionDemo {
+
+    public static void main(String[] args) {
+
+    Vehicle  c1 = new Car();
+    c1.start();
+    c1.stop();
+
+    Vehicle b1 = new Bike();
+    b1.start();
+    b1.stop();
+
+
+    }
+}
