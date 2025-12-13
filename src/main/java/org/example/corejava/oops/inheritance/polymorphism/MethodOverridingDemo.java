3a1ddebc996f36f47c918b00db3ae3087dffea08
Sumit Vishwakarma <sv0042595@gmail.com>
2025-12-13 14:01:33 +0530

    OOPS Concepts

--- src/main/java/org/example/corejava/oops/inheritance/polymorphism/MethodOverridingDemo.java
+++ src/main/java/org/example/corejava/oops/inheritance/polymorphism/MethodOverridingDemo.java
@@ -1,4 +1,25 @@
 package org.example.corejava.oops.inheritance.polymorphism;
 
+class Shape{
+    void draw(){
+        System.out.println("Drawing Shape...");
+    }
+}
+
+class Circle extends Shape{
+    @Override
+    void draw(){
+        System.out.println("Drawing Circle...");
+    }
+}
+
 public class MethodOverridingDemo {
+
+    public static void main(String[] args) {
+
+        Shape s1 = new Shape();
+        s1.draw();
+
+
+    }
 }
