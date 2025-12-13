7d26fc5b637a038177e30ac46deced58f391c5bd
Sumit Vishwakarma <sv0042595@gmail.com>
2025-12-13 13:05:24 +0530

    OOPS Concepts

--- src/main/java/org/example/corejava/oops/inheritance/SingleInheriDemo.java
+++ src/main/java/org/example/corejava/oops/inheritance/SingleInheriDemo.java
@@ -1,4 +1,22 @@
 package org.example.corejava.oops.inheritance;
 
+
+class Info{
+
+    String name = "Sumit";
+    String std = "B TECH";
+
+    public void name(){
+        System.out.println(this.name);
+    }
+
+}
+
 public class SingleInheriDemo {
+
+    public static void main(String args[]){
+
+//        Single Level Inheritance
+
+    }
 }
