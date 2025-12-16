1cedc72780bce7e017f7dcca02b1f4f6dbed4050
Sumit Vishwakarma <sv0042595@gmail.com>
2025-12-16 15:40:09 +0530

    Exception Handling

--- src/main/java/org/example/corejava/exceptionhanding/DemoException.java
+++ src/main/java/org/example/corejava/exceptionhanding/DemoException.java
@@ -1,9 +1,18 @@
 package org.example.corejava.exceptionhanding;
 
+import java.io.IOException;
+
 public class DemoException {
 
     public static void main(String[] args) {
 
-        
+        try{
+            throw new IOException("Req Fails Due to IO Exception");
+        }catch (Exception e){
+//            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+
     }
 }
