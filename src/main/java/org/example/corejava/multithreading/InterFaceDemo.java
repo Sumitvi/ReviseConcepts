aa630bde12dca5c5b3ccf29f624176e202259d5c
Sumit Vishwakarma <sv0042595@gmail.com>
2025-12-16 16:33:14 +0530

    MultiThreading

--- src/main/java/org/example/corejava/multithreading/InterFaceDemo.java
+++ src/main/java/org/example/corejava/multithreading/InterFaceDemo.java
@@ -1,8 +1,21 @@
 package org.example.corejava.multithreading;
 
+interface Animal{
+    void sound();
+}
+
+class Dog implements Animal{
+    @Override
+    public void sound(){
+        System.out.println("Dog Bark");
+    }
+}
+
 public class InterFaceDemo {
 
     public static void main(String[] args) {
-        
+
+
+
     }
 }
