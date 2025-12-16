e4605f433a1851df1dcb1726b0aea0406b339b15
Sumit Vishwakarma <sv0042595@gmail.com>
2025-12-16 12:13:31 +0530

    Java Concepts Revision

--- src/main/java/org/example/corejava/concepts/KeywordsAndVariables/KeyWordDemo.java
+++ src/main/java/org/example/corejava/concepts/KeywordsAndVariables/KeyWordDemo.java
@@ -1,4 +1,16 @@
 package org.example.corejava.concepts.KeywordsAndVariables;
 
 public class KeyWordDemo {
+
+    public static void main(String[] args) {
+
+//        Final Keyword
+//        When we declare something final we can'nt chnage those
+
+        final int  a  = 20;
+        System.out.println(a);
+//        a = 30;
+
+
+    }
 }
