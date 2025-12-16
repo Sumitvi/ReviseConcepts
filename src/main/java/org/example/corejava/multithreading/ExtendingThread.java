71e203e50527a48d66fb9270ce9d01650a7841ef
Sumit Vishwakarma <sv0042595@gmail.com>
2025-12-16 15:59:08 +0530

    MultiThreading

---
+++ src/main/java/org/example/corejava/multithreading/ExtendingThread.java
@@ -0,0 +1,14 @@
+package org.example.corejava.multithreading;
+
+public class ExtendingThread {
+
+    public static void main(String[] args) {
+
+//        Executing multiple threads simultaneously
+//        within a single process is called multithreading.
+
+        
+    }
+
+
+}
