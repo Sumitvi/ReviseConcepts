f87a8dfe0af94d341422507a5e72b634abcac33f
Sumit Vishwakarma <sv0042595@gmail.com>
2025-12-16 16:38:46 +0530

    MultiThreading

--- src/main/java/org/example/corejava/multithreading/UsingRunnable.java
+++ src/main/java/org/example/corejava/multithreading/UsingRunnable.java
@@ -1,8 +1,21 @@
 package org.example.corejava.multithreading;
 
+class MyNewThread implements Runnable{
+
+    @Override
+    public void run(){
+        for(int i=0; i<=5; i++){
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
+    }
+}
 public class UsingRunnable {
 
     public static void main(String[] args) {
-        
+        MyNewThread r = new MyNewThread();
+        Thread t1 = new Thread(r);
+        t1.setName("Thread-1");
+        t1.start();
+
     }
 }
