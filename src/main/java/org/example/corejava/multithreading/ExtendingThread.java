e1864303e6a23b9746650764ee2a7b922d7cc05b
Sumit Vishwakarma <sv0042595@gmail.com>
2025-12-16 16:23:43 +0530

    MultiThreading

--- src/main/java/org/example/corejava/multithreading/ExtendingThread.java
+++ src/main/java/org/example/corejava/multithreading/ExtendingThread.java
@@ -6,6 +6,9 @@ class MyThread extends Thread{
     @Override
     public void run(){
 
+        for(int i=0; i<=5; i++){
+            System.out.println(Thread.currentThread().getName() +" "+ i);
+        }
     }
 }
 
@@ -17,6 +20,15 @@ public class ExtendingThread {
 
 //        Executing multiple threads simultaneously
 //        within a single process is called multithreading.
+
+        MyThread t1 = new MyThread();
+        MyThread t2 = new MyThread();
+
+        t1.setName("Thread-1");
+        t2.setName("Thread-2");
+
+        t1.start();
+        t2.start();
 
 
     }
