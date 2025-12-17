ba3620ae3905210dcd71bdc66e3e0bfddedb5e8d
Sumit Vishwakarma <sv0042595@gmail.com>
2025-12-17 13:03:48 +0530

    Collection

--- src/main/java/org/example/corejava/collections/ListDemo.java
+++ src/main/java/org/example/corejava/collections/ListDemo.java
@@ -1,4 +1,25 @@
 package org.example.corejava.collections;
+import java.util.*;
 
 public class ListDemo {
+
+    public static void main(String args[]){
+
+        ArrayList<Integer> res = new ArrayList<>();
+//        Adding Number in List
+        res.add(1);
+        res.add(2);
+        res.add(3);
+        res.add(4);
+        res.add(1);
+
+        System.out.println(res);
+
+//        Checking empty List
+
+        if(res.isEmpty()){
+            System.out.println("List is Empty");
+        }
+
+    }
 }
