f217172cf037627211acecc6aa60f91c27f19e42
Sumit Vishwakarma <sv0042595@gmail.com>
2025-12-18 11:33:24 +0530

    Collection

--- src/main/java/org/example/corejava/collections/HashMapDemo.java
+++ src/main/java/org/example/corejava/collections/HashMapDemo.java
@@ -1,4 +1,24 @@
 package org.example.corejava.collections;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HashMapDemo {
+
+    public static void main(String[] args) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0 , 1);
+        map.put(1 , 1);
+        map.put(2 , 1);
+        map.put(3 , 1);
+        map.put(4 , 1);
+
+        System.out.println(map);
+
+        for(Map.Entry<Integer, Integer> entry : map.entrySet() ){
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+
+    }
 }
