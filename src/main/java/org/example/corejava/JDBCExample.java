51af9ae50d050f11ed4d7174d210f13e815572f9
Sumit Vishwakarma <sv0042595@gmail.com>
2025-12-18 13:52:54 +0530

    JDBC

--- src/main/java/org/example/corejava/JDBCExample.java
+++ src/main/java/org/example/corejava/JDBCExample.java
@@ -1,4 +1,14 @@
 package org.example.corejava;
 
 public class JDBCExample {
+
+    public static String LOAD_DRIVER = "com.mysql.cj.jdbc.Driver";
+    public static String URL = "jdbc:mysql://localhost:3306/testdb";
+    public static String PASSWORD = "Sumit@2004";
+    public static String USERNAME = "root";
+
+
+    public static void main(String[] args) {
+
+    }
 }
