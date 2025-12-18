d92d39e4e2983a46f57d8ab978829d956ea6d938
Sumit Vishwakarma <sv0042595@gmail.com>
2025-12-18 11:20:08 +0530

    Collection

--- src/main/java/org/example/corejava/collections/SetDemo.java
+++ src/main/java/org/example/corejava/collections/SetDemo.java
@@ -1,7 +1,26 @@
 package org.example.corejava.collections;
 
+import java.util.*;
+
 public class SetDemo {
     public static void main(String args[]){
-        
+
+//        pro - not an index based and duplicate ele not allowed
+        HashSet<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(4);
+        set.add(1);
+
+        System.out.println(set);
+
+        for(int data : set){
+            System.out.println(data);
+        }
+
+
+
+
     }
 }
