c56aec1c47a01cd2bc1c1c16c9ecbe3b8c48ab66
Sumit Vishwakarma <sv0042595@gmail.com>
2025-12-18 11:22:22 +0530

    Collection

--- src/main/java/org/example/corejava/collections/SetDemo.java
+++ src/main/java/org/example/corejava/collections/SetDemo.java
@@ -11,13 +11,21 @@ public class SetDemo {
         set.add(2);
         set.add(3);
         set.add(4);
-        set.add(1);
+        set.add(0);
 
         System.out.println(set);
 
         for(int data : set){
             System.out.println(data);
         }
+
+        HashSet<String> names = new HashSet<>();
+        names.add("Sumit");
+        names.add("sUmit");
+        names.add("suMit");
+        names.add("sumIt");
+
+        System.out.println(names);
 
 
 
